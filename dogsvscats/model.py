743fc275673abed1e246315844a9e1f1d3a6bc25
Tommy Engstrom <tommy@tommyengstrom.com>
2017-08-06 12:22:31 +0200

    It runs

--- dogsvscats/model.py
+++ dogsvscats/model.py
@@ -2,6 +2,7 @@ import keras
 from keras.applications.vgg16 import VGG16
 from keras.models import Sequential
 import keras.models as M
+import keras.optimizers as O
 import keras.layers as L
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
@@ -9,8 +10,8 @@ def main():
 
     train_gen, val_gen = data_gen(use_sample=True)
     model = build_model()
-    import pudb;pudb.set_trace()
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy')
+    # import pudb;pudb.set_trace()
+    model.compile(optimizer=O.rmsprop(lr=0.0001), loss='binary_crossentropy', metrics=['accuracy'])
     model.fit_generator(
         train_gen,
         steps_per_epoch=100,
@@ -18,8 +19,9 @@ def main():
         validation_data=val_gen,
         validation_steps=50
         )
+    import pudb; pudb.set_trace()
 
-def data_gen(use_sample=False, batch_size=16, target_size=(224, 224)):
+def data_gen(use_sample=False, batch_size=8, target_size=(224, 224)):
     base_path = 'data/sample/' if use_sample else 'data/full'
     train_generator = ImageDataGenerator(rescale=1/255)
     test_generator = ImageDataGenerator(rescale=1/255)
@@ -41,10 +43,10 @@ def data_gen(use_sample=False, batch_size=16, target_size=(224, 224)):
 
 
 def build_model():
-    vgg = VGG16(weights='imagenet', include_top=False)
-    import pudb; pudb.set_trace()
+    vgg = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
     m = L.Flatten()(vgg.output)
-    m = L.Dense(2, activation='softmax')(m)
+    m = L.Dense(100, activation='relu')(m)
+    m = L.Dense(1, activation='softmax')(m)
     return M.Model(vgg.input, m)
 
 # initial_model = VGG16(weights="imagenet", include_top=False)
