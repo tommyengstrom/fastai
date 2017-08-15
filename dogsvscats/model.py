7f9cfba4fe7f7c19585490c358cd74b13ac9af3e
Tommy Engstrom <tommy@hihenry.co>
2017-08-15 06:52:53 +0200

    make it prettier

--- dogsvscats/model.py
+++ dogsvscats/model.py
@@ -1,16 +1,14 @@
-import keras
 from keras.applications.vgg16 import VGG16
-from keras.models import Sequential
 import keras.models as M
 import keras.optimizers as O
 import keras.layers as L
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
 
 def main():
+    img_size = (224, 224)
 
-    train_gen, val_gen = data_gen(use_sample=False)
-    model = build_model()
-    # import pudb;pudb.set_trace()
+    train_gen, val_gen = data_gen(use_sample=True, img_size=img_size)
+    model = build_vgg_model(img_size=img_size)
     model.compile(optimizer=O.rmsprop(lr=0.001),
                   loss='binary_crossentropy',
                   metrics=['accuracy'])
@@ -24,29 +22,28 @@ def main():
     model.save_weights('model_weights.hdf5')
     import pudb; pudb.set_trace()
 
-def data_gen(use_sample=False, batch_size=8, target_size=(224, 224)):
+def data_gen(img_size, use_sample=False, batch_size=16):
     base_path = 'data/sample/' if use_sample else 'data/full/'
-    train_generator = ImageDataGenerator() #rescale=1/255)
-    test_generator = ImageDataGenerator() #rescale=1/255)
+    image_generator = ImageDataGenerator() #rescale=1/255)
 
-    train_gen = train_generator.flow_from_directory(
+    train_gen = image_generator.flow_from_directory(
         base_path + 'train/',
-        target_size=target_size,
+        target_size=img_size,
         batch_size=batch_size,
         class_mode='binary'
         )
-    val_gen = test_generator.flow_from_directory(
+    val_gen = image_generator.flow_from_directory(
         base_path + 'validation/',
-        target_size=target_size,
+        target_size=img_size,
         batch_size=batch_size,
         class_mode='binary'
        )
     return train_gen, val_gen
 
 
-def build_simple_model():
-    return Sequential(
-        [ L.Conv2D(32, (3, 3), input_shape=(224, 224, 3))
+def build_simple_model(img_size):
+    return M.Sequential(
+        [ L.Conv2D(32, (3, 3), input_shape=img_size + (3,))
         , L.Activation('relu')
         , L.MaxPooling2D(pool_size=(2, 2))
         , L.Conv2D(32, (3, 3))
@@ -61,9 +58,9 @@ def build_simple_model():
         , L.Dense(1, activation='softmax')
         ])
 
-def build_model():
-    # vgg = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
-    vgg = VGG16(weights='imagenet', include_top=True, input_shape=(224,224,3))
+def build_vgg_model(img_size):
+    # vgg = VGG16(weights='imagenet', include_top=False, input_shape=img_size + (3,))
+    vgg = VGG16(weights='imagenet', include_top=True, input_shape=img_size + (3,))
     for layer in vgg.layers:
         layer.trainable = False
     # m = L.Flatten()(vgg.output)
