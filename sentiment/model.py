f4423de2cf7630882684de7304bb0ce82ae2bc05
Tommy Engstrom <tommy@hihenry.co>
2017-10-10 08:42:43 +0200

    It doens't work

--- sentiment/model.py
+++ sentiment/model.py
@@ -2,16 +2,45 @@ import keras.layers as L
 import keras.models as M
 import keras.optimizers as O
 from keras.datasets import imdb
+from keras.preprocessing import sequence
+from keras.models import Sequential
+from keras.layers.embeddings import Embedding
 
-def main():
+seq_len = 200
+maxfeatures = 10000
+
+def load_data():
     (x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
-                                                          num_words=None,
+                                                          num_words=maxfeatures -1,
                                                           skip_top=0,
-                                                          maxlen=None,
+                                                          maxlen=seq_len,
                                                           seed=113,
                                                           start_char=1,
                                                           oov_char=2,
                                                           index_from=3)
+    pad = sequence.pad_sequences
+    return ((pad(x_train, maxlen=seq_len),y_train),
+            (pad(x_test,maxlen=seq_len), y_test))
+
+def build_model():
+    return Sequential([Embedding(maxfeatures, 25,input_length=seq_len),
+                      L.Dense(100),
+                      L.Dense(1),
+                      L.Activation('sigmoid')
+                      ])
+
+def main():
+    (x_train, y_train), (x_test, y_test) = load_data()
+    model = build_model()
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print(x_train[0])
+    print(x_test)
+    print(len(x_train), len(x_test))
+    print(len(x_train[0]), len(x_test[0]))
+    model.fit(x_train, y_train,
+          batch_size=30,
+          epochs=2,
+          validation_data=(x_test, y_test))
 
 if __name__ == "__main__":
     main()
